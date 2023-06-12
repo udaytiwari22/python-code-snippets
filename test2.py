453a19660df4994c84f8815eef2ef222cac8bd06
Uday <udaytiwaricyef2001@gmail.com>
2023-06-13 03:18:26 +0530

    Lists

--- test2.py
+++ test2.py
@@ -1,6 +1,14 @@
-a= int(input())
-for i in range(2,a+1,1):
-    if(i%7==0):
-        continue
-    else:
-        print(i)
+li =[1,2,4.5,'a',2,"Uday"]
+# print(li)
+# li[4]="Mitsuri"
+# print(li)
+# li.insert(1,5)
+# print(li)
+# li.append([1,2,3])
+# print(li)
+# li.extend([3,4,5,6,7,2,3,6.7,9,'no'])
+# print(li)
+# li.remove(2,4.5)
+# print(li)
+li.pop(3,4,5)
+print(li)
